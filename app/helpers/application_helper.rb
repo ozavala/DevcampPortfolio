12861e5ec4aff037a836eb1ba5f7b0d35adf3e3c
Vagrant <oszavala@gmail.com>
2018-10-02 19:33:58 +0000

    Implemented index styles for blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,16 @@ module ApplicationHelper
     end
   end
 
+  # def alerts
+  #   alert = (flash[:alert] || flash[:error] || flash[:notice])
+  #
+  #   if alert
+  #     alert_generator alert
+  #   end
+  # end
+  #
+  # def alert_generator msg
+  #   js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
+  # end
+
 end
