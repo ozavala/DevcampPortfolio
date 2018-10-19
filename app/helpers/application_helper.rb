609e2ee89ae8818c01ddac336496f927fefd7e61
ozavala <oszavala@gmail.com>
2018-10-19 13:11:27 -0500

    refactored alets into helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,14 +57,14 @@ module ApplicationHelper
     "active" if current_page? path
   end
 end
-  # def alerts
-  #   alert = (flash[:alert] || flash[:error] || flash[:notice])
+   def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
   #
-  #   if alert
-  #     alert_generator alert
-  #   end
-  # end
-  #
-  # def alert_generator msg
-  #   js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
-  # end
+   def alert_generator msg
+     js add_gritter(msg, title: "Oswaldo Zavala Portfolio", sticky: false)
+   end
