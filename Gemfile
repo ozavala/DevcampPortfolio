a8aa92babf5f230ad1a4650ae63123b7307edad1
Oswaldo Zavala <oszavala@gmail.com>
2019-02-11 14:01:59 -0500

    Implemented custom scopes for portfolios items"

--- Gemfile
+++ Gemfile
@@ -13,6 +13,12 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
 
+
+gem "rack", ">= 2.0.6"
+gem "loofah", ">= 2.2.3"
+
+
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry-byebug'
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,34 +22,34 @@ puts "10 Blogs created"
 end
 puts "5 Skills items created"
 
-# 8.times do |portfolio_item|
-#   Portfolio.create!(
-#     title: "Portfolio Title #{portfolio_item}",
-#     subtitle: "Ruby on Rails",
-#     body: "Nam pulvinar nisi dui, nec tristique dui tincidunt at. Phasellus lacinia felis nec varius efficitur. Suspendisse id semper leo. Nunc tristique sem in quam lacinia maximus. Proin auctor arcu mi, at lobortis est auctor in. Quisque sed pharetra nisi. In vitae tellus a velit condimentum lobortis. Integer sit amet ornare lacus. Donec nulla dui, dignissim dignissim sem sit amet, euismod tristique tortor. ",
-#     main_image: "https://via.placeholder.com/600x400",
-#     thumb_image: "https://via.placeholder.com/350x200"
-#    )
-#  end
-#
-#  1.times do |portfolio_item|
-#    Portfolio.create!(
-#      title: "Portfolio Title #{portfolio_item}",
-#      subtitle: "Angular",
-#      body: "Nam pulvinar nisi dui, nec tristique dui tincidunt at. Phasellus lacinia felis nec varius efficitur. Suspendisse id semper leo. Nunc tristique sem in quam lacinia maximus. Proin auctor arcu mi, at lobortis est auctor in. Quisque sed pharetra nisi. In vitae tellus a velit condimentum lobortis. Integer sit amet ornare lacus. Donec nulla dui, dignissim dignissim sem sit amet, euismod tristique tortor. ",
-#      main_image: "https://via.placeholder.com/600x400",
-#      thumb_image: "https://via.placeholder.com/350x200"
-#     )
-#   end
-#  puts "9 Portfolio Items made"
-#
-#  # 3.times do |technology|
-#  #   Tecnology.create!(
-#  #     name: "Technology #{technology}",
-#  #     portfolio_id: Portfolio.last.id
-#  #   )
-#  #  end
-#
+8.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Ruby on Rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Angular",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    main_image: "http://placehold.it/600x400",
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts "9 portfolio items created"
+ # 3.times do |technology|
+ #   Tecnology.create!(
+ #     name: "Technology #{technology}",
+ #     portfolio_id: Portfolio.last.id
+ #   )
+ #  end
+
 #   3.times do |technology|
 #     Portfolio.last.technologies.create!(
 #       name: "Technology #{technology}"
